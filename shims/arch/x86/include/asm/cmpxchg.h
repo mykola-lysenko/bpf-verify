a59b16a69e457bb8b31b9ce411ff921996fb84f0
mykolal <mykolal@devvm50421.cco0.facebook.com>
2026-05-08 22:14:32 -0700

    Model cmpxchg with plain C operations

--- shims/arch/x86/include/asm/cmpxchg.h
+++ shims/arch/x86/include/asm/cmpxchg.h
@@ -1,39 +1,54 @@
 /* BPF shim: asm/cmpxchg.h
  * x86 cmpxchg.h uses LOCK CMPXCHG inline asm not valid in BPF.
- * Provide BPF-safe implementations using __sync builtins.
+ * Model exchange operations as plain load/compare/store sequences. BPF
+ * verification is single-threaded, so atomic ordering is not relevant here.
  */
 #ifndef _ASM_X86_CMPXCHG_H
 #define _ASM_X86_CMPXCHG_H
 
-/* arch_xchg: atomically exchange *ptr with val, return old value */
-#define arch_xchg(ptr, val) \
-__sync_lock_test_and_set(ptr, val)
-
-/* arch_cmpxchg: compare and exchange */
-#define arch_cmpxchg(ptr, old, new) \
-__sync_val_compare_and_swap(ptr, old, new)
-
-/* arch_try_cmpxchg: try compare and exchange, update *old on failure */
-#define arch_try_cmpxchg(ptr, pold, new) \
-({ \
-typeof(*(ptr)) __old = *(pold); \
-typeof(*(ptr)) __prev = __sync_val_compare_and_swap(ptr, __old, new); \
-if (__prev != __old) *(pold) = __prev; \
-(__prev == __old); \
+#define arch_xchg(ptr, val)					\
+({								\
+	typeof(*(ptr)) *__ptr = (ptr);				\
+	typeof(*__ptr) __old = *__ptr;				\
+	*__ptr = (typeof(*__ptr))(val);				\
+	__old;							\
 })
 
-/* arch_cmpxchg64: 64-bit compare and exchange */
-#define arch_cmpxchg64(ptr, old, new) \
-__sync_val_compare_and_swap(ptr, old, new)
+#define arch_cmpxchg(ptr, old, new)				\
+({								\
+	typeof(*(ptr)) *__ptr = (ptr);				\
+	typeof(*__ptr) __old = (old);				\
+	typeof(*__ptr) __prev = *__ptr;				\
+	if (__prev == __old)					\
+		*__ptr = (typeof(*__ptr))(new);			\
+	__prev;							\
+})
 
-/* arch_xchg_relaxed, arch_cmpxchg_relaxed, arch_cmpxchg_acquire, arch_cmpxchg_release */
-#define arch_xchg_relaxed(ptr, val)arch_xchg(ptr, val)
-#define arch_cmpxchg_relaxed(ptr, old, new)arch_cmpxchg(ptr, old, new)
-#define arch_cmpxchg_acquire(ptr, old, new)arch_cmpxchg(ptr, old, new)
-#define arch_cmpxchg_release(ptr, old, new)arch_cmpxchg(ptr, old, new)
-#define arch_try_cmpxchg_relaxed(ptr, pold, new) arch_try_cmpxchg(ptr, pold, new)
-#define arch_try_cmpxchg_acquire(ptr, pold, new) arch_try_cmpxchg(ptr, pold, new)
-#define arch_try_cmpxchg_release(ptr, pold, new) arch_try_cmpxchg(ptr, pold, new)
+#define arch_try_cmpxchg(ptr, pold, new)			\
+({								\
+	typeof(*(ptr)) *__ptr = (ptr);				\
+	typeof(*__ptr) *__oldp = (pold);			\
+	typeof(*__ptr) __old = *__oldp;				\
+	typeof(*__ptr) __prev = *__ptr;				\
+	if (__prev == __old)					\
+		*__ptr = (typeof(*__ptr))(new);			\
+	else							\
+		*__oldp = __prev;				\
+	__prev == __old;					\
+})
+
+#define arch_cmpxchg64(ptr, old, new)	arch_cmpxchg(ptr, old, new)
+
+#define arch_xchg_relaxed(ptr, val)		arch_xchg(ptr, val)
+#define arch_cmpxchg_relaxed(ptr, old, new)	arch_cmpxchg(ptr, old, new)
+#define arch_cmpxchg_acquire(ptr, old, new)	arch_cmpxchg(ptr, old, new)
+#define arch_cmpxchg_release(ptr, old, new)	arch_cmpxchg(ptr, old, new)
+#define arch_try_cmpxchg_relaxed(ptr, pold, new) \
+	arch_try_cmpxchg(ptr, pold, new)
+#define arch_try_cmpxchg_acquire(ptr, pold, new) \
+	arch_try_cmpxchg(ptr, pold, new)
+#define arch_try_cmpxchg_release(ptr, pold, new) \
+	arch_try_cmpxchg(ptr, pold, new)
 
 /* cmpxchg_double stubs */
 #define arch_cmpxchg_double(p1, p2, o1, o2, n1, n2) (0)
