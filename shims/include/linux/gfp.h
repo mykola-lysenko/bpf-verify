f086e18be0a0839418d8f4354dae6efe2f88c835
mykolal <mykolal@devvm50421.cco0.facebook.com>
2026-05-09 08:22:20 -0700

    Simplify GFP shim stubs

--- shims/include/linux/gfp.h
+++ shims/include/linux/gfp.h
@@ -15,47 +15,26 @@ struct zonelist;
 struct mem_cgroup;
 
 #define __bpf_gfp_noop(...)		do { } while (0)
+#define __bpf_gfp_null(...)		((void *)0)
+#define __bpf_gfp_alloc_stub(type, name, ...)		\
+	static inline type name(__VA_ARGS__)		\
+	{						\
+		return (type)__bpf_gfp_null();		\
+	}
+#define __bpf_gfp_free_stub(name, ...)			\
+	static inline void name(__VA_ARGS__) { }
 
 /* Minimal allocation stubs - BPF programs don't actually allocate memory. */
-static inline struct page *alloc_pages(gfp_t gfp, unsigned int order)
-{
-	__bpf_gfp_noop(gfp, order);
-	return (void *)0;
-}
-
-static inline struct page *alloc_pages_node(int nid, gfp_t gfp,
-					    unsigned int order)
-{
-	__bpf_gfp_noop(nid);
-	return alloc_pages(gfp, order);
-}
-
-static inline void __free_pages(struct page *page, unsigned int order)
-{
-	__bpf_gfp_noop(page, order);
-}
-
-static inline void *alloc_pages_exact(size_t size, gfp_t gfp)
-{
-	__bpf_gfp_noop(size, gfp);
-	return (void *)0;
-}
-
-static inline void free_pages_exact(void *addr, size_t size)
-{
-	__bpf_gfp_noop(addr, size);
-}
-
-static inline struct folio *folio_alloc(gfp_t gfp, unsigned int order)
-{
-	__bpf_gfp_noop(gfp, order);
-	return (void *)0;
-}
-
-static inline void folio_put(struct folio *folio)
-{
-	__bpf_gfp_noop(folio);
-}
+__bpf_gfp_alloc_stub(struct page *, alloc_pages,
+		     gfp_t gfp, unsigned int order)
+__bpf_gfp_alloc_stub(struct page *, alloc_pages_node,
+		     int nid, gfp_t gfp, unsigned int order)
+__bpf_gfp_free_stub(__free_pages, struct page *page, unsigned int order)
+__bpf_gfp_alloc_stub(void *, alloc_pages_exact, size_t size, gfp_t gfp)
+__bpf_gfp_free_stub(free_pages_exact, void *addr, size_t size)
+__bpf_gfp_alloc_stub(struct folio *, folio_alloc,
+		     gfp_t gfp, unsigned int order)
+__bpf_gfp_free_stub(folio_put, struct folio *folio)
 
 #define alloc_page(gfp)			alloc_pages((gfp), 0)
 #define __free_page(page)		__free_pages((page), 0)
@@ -77,7 +56,7 @@ static inline void folio_put(struct folio *folio)
 /* alloc_tag stubs - normally in sched.h but we block sched.h */
 struct alloc_tag;
 #ifndef alloc_tag_save
-#define alloc_tag_save(_tag)		((void *)0)
+#define alloc_tag_save(_tag)		__bpf_gfp_null(_tag)
 #define alloc_tag_restore(_tag, _old)	__bpf_gfp_noop(_tag, _old)
 #endif
 
